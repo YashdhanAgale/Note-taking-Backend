b4dbebfbb8247119a585bf83607388c8c3ac4fca
yashodhanAgale <yashodhanagale3@gmail.com>
2025-07-28 02:53:16 +0530

    resolving cookie issue

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -67,8 +67,8 @@ export const signin = async (req: Request, res: Response) => {
     // Store token in cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure:false,
-      sameSite: "lax",
+      secure:true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
