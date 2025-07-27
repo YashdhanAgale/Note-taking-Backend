259212fdf6e5fa851d8595dafedc57033e22b75c
yashodhanAgale <yashodhanagale3@gmail.com>
2025-07-28 01:21:24 +0530

    deployment

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,24 @@ import noteRoutes from './routes/noteRoutes';
 const app = express();
 dotenv.config();
 
-const allowedOrigins = ['https://note-taking-frontend-pied.vercel.app']; 
+const allowedOrigins = ['https://note-taking-frontend-pied.vercel.app'];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
+app.options('*', cors()); 
 
 app.use(express.json());
 app.use(cookieParser());
